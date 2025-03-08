042cbed7926fb75244515bc386119a634d1ff472
muhammadasif-cse <muhammadasif.cse@gmail.com>
2025-03-08 12:56:10 +0600

    refactor: move global styles to a new file and update import path

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "./styles/globals.css";
 
 const interSans = Inter({
   variable: "--font-inter-sans",
