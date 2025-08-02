64d08cc13843246dc2de42bb67cca52bd0886bfa
muhammadasif-cse <muhammadasif.cse@gmail.com>
2025-08-02 19:34:44 +0600

    feat: implement theme provider and enhance navigation with theme toggle functionality

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
+      <head />
       <body className={`${poppinsSans.variable} antialiased`}>
         <ReduxProviders>
           <Navigation />
